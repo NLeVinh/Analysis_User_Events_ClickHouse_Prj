0b5277d802dd5f2b8b9863f8ff8ac4311103bdd6
NLeVinh <nguyenvinh120330@gmail.com>
2026-03-30 10:43:07 +0700

    fix script

--- scripts/replay_events.py
+++ scripts/replay_events.py
@@ -3,6 +3,7 @@ import clickhouse_connect
 import time
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 # ==========================
 # CONFIG
@@ -23,7 +24,24 @@ CSV_FILE = os.getenv("CSV_FILE")
 CHUNK_SIZE = int(os.getenv("CHUNK_SIZE"))      # rows read from CSV
 BATCH_SIZE = int(os.getenv("BATCH_SIZE"))       # rows per insert
 SLEEP_TIME = float(os.getenv("SLEEP_TIME"))       # simulate streaming
+MAX_RECORDS = int(os.getenv("MAX_RECORDS"))
 
+# print("CLICKHOUSE CONFIG:")
+# print("HOST:", CLICKHOUSE_HOST)
+# print("PORT:", CLICKHOUSE_PORT, type(CLICKHOUSE_PORT))
+# print("USER:", CLICKHOUSE_USER)
+# print("PASSWORD:", CLICKHOUSE_PASSWORD)
+# print("DATABASE:", CLICKHOUSE_DATABASE)
+# print("TABLE_NAME:", TABLE_NAME)
+
+# print("\nDATA CONFIG:")
+# print("CSV_FILE:", CSV_FILE)
+
+# print("\nPROCESSING CONFIG:")
+# print("CHUNK_SIZE:", CHUNK_SIZE, type(CHUNK_SIZE))
+# print("BATCH_SIZE:", BATCH_SIZE, type(BATCH_SIZE))
+# print("SLEEP_TIME:", SLEEP_TIME, type(SLEEP_TIME))
+# print("MAX_RECORDS:", MAX_RECORDS, type(MAX_RECORDS))
 
 # ==========================
 # CONNECT CLICKHOUSE
@@ -39,69 +57,91 @@ client = clickhouse_connect.get_client(
 
 print("Connected to ClickHouse")
 
+dtypes = {
+    "event_type": "category",
+    "product_id": "int64",
+    "category_id": "int64",
+    "category_code": "string",
+    "brand": "string",
+    "price": "float32",
+    "user_id": "int64",
+    "user_session": "string"
+}
 
-# # ==========================
-# # INSERT FUNCTION
-# # ==========================
+columns = [
+    "event_time",
+    "event_type",
+    "product_id",
+    "category_id",
+    "category_code",
+    "brand",
+    "price",
+    "user_id",
+    "user_session"
+]
 
-# def insert_batch(df):
+# ==========================
+# STREAM + INSERT
+# ==========================
+total_inserted = 0
 
-#     records = df.to_dict("records")
+df_iter = pd.read_csv(
+    CSV_FILE,
+    chunksize=CHUNK_SIZE,
+    dtype=dtypes,
+    usecols=columns
+)
+for chunk_idx, chunk in enumerate(df_iter):
 
-#     client.insert(
-#         f"{CLICKHOUSE_DATABASE}.{TABLE_NAME}",
-#         records,
-#         column_names=[
-#             "event_time",
-#             "event_type",
-#             "product_id",
-#             "category_id",
-#             "category_code",
-#             "brand",
-#             "price",
-#             "user_id",
-#             "user_session"
-#         ]
-#     )
+    # ===== CLEAN DATA =====
+    chunk["category_code"] = chunk["category_code"].fillna("")
+    chunk["brand"] = chunk["brand"].fillna("")
 
+    chunk["category_id"] = chunk["category_id"].fillna(0)
+    chunk["product_id"] = chunk["product_id"].fillna(0)
+    chunk["user_id"] = chunk["user_id"].fillna(0)
 
-# # ==========================
-# # REPLAY CSV
-# # ==========================
+    # đảm bảo datetime
+    chunk["event_time"] = pd.to_datetime(chunk["event_time"])
 
-# total_inserted = 0
+    # print(pd.to_numeric(chunk["category_id"], errors="coerce").max() > 2**64 - 1)
+    # print(chunk["category_id"].min())
 
-# for chunk in pd.read_csv(CSV_FILE, chunksize=CHUNK_SIZE):
+    # ===== SPLIT INTO BATCH =====
+    for i in range(0, len(chunk), BATCH_SIZE):
 
-#     # convert datetime
-#     chunk["event_time"] = pd.to_datetime(chunk["event_time"])
+        batch = chunk.iloc[i:i+BATCH_SIZE]
 
-#     # reorder columns to match schema
-#     chunk = chunk[
-#         [
-#             "event_time",
-#             "event_type",
-#             "product_id",
-#             "category_id",
-#             "category_code",
-#             "brand",
-#             "price",
-#             "user_id",
-#             "user_session"
-#         ]
-#     ]
+        # LIMIT theo MAX_RECORDS
+        if MAX_RECORDS > 0:
+            remaining = MAX_RECORDS - total_inserted
+            if remaining <= 0:
+                break
+            batch = batch.iloc[:remaining]
 
-#     # split into batches
-#     for i in range(0, len(chunk), BATCH_SIZE):
+        # convert → list of tuple (nhanh nhất cho insert)
+        data = list(batch.itertuples(index=False, name=None))
 
-#         batch = chunk.iloc[i:i+BATCH_SIZE]
+        # ===== INSERT =====
+        client.insert(
+            TABLE_NAME,
+            data,
+            column_names=columns
+        )
 
-#         insert_batch(batch)
+        total_inserted += len(data)
 
-#         total_inserted += len(batch)
+        print(f"[Chunk {chunk_idx}] Inserted: {total_inserted}")
 
-#         print(f"Inserted rows: {total_inserted}")
+        # ===== STOP CONDITION =====
+        if MAX_RECORDS > 0 and total_inserted >= MAX_RECORDS:
+            print("Reached MAX_RECORDS. STOP")
+            break
 
-#         time.sleep(SLEEP_TIME)
+        # ===== SIMULATE STREAM =====
+        time.sleep(SLEEP_TIME)
 
-# print("Replay completed")
+    if MAX_RECORDS > 0 and total_inserted >= MAX_RECORDS:
+        break
+
+print("Replay completed")
